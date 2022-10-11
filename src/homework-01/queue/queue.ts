6acc07bd4a1f1f873629f21a8dce65ac6bf81dff
Дмитрий Лебедко <Lebedko.D.Y@sber-tech.com>
2022-10-11 11:43:03 +0300

    Fixed field name for base structure

--- src/homework-01/queue/queue.ts
+++ src/homework-01/queue/queue.ts
@@ -2,20 +2,20 @@ import DoublyLinkedList from '../doubly-linked-list/doubly-linked-list';
 import type Queue from './queue.interface';
 
 export default class QueueImpl<T = unknown> implements Queue<T> {
-  #queue: DoublyLinkedList<T> = new DoublyLinkedList<T>();
+  #list: DoublyLinkedList<T> = new DoublyLinkedList<T>();
 
   get isEmpty() {
-    return this.#queue.isEmpty;
+    return this.#list.isEmpty;
   }
 
   enqueue(value: T): this {
-    this.#queue.unshift(value);
+    this.#list.unshift(value);
 
     return this;
   }
 
   dequeue() {
-    const firstNode = this.#queue.pop();
+    const firstNode = this.#list.pop();
 
     if (firstNode === null) {
       throw new Error('Queue is empty');
@@ -25,6 +25,6 @@ export default class QueueImpl<T = unknown> implements Queue<T> {
   }
 
   peek() {
-    return this.#queue.tail?.value ?? null;
+    return this.#list.tail?.value ?? null;
   }
 }
