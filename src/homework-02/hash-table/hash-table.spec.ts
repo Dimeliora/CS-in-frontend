eb2c93d140842981d75137d67d740311837d220c
Dimeliora <dimeliora710@gmail.com>
2022-10-16 11:16:17 +0300

    Finished hash table implementation & unit tests

--- src/homework-02/hash-table/hash-table.spec.ts
+++ src/homework-02/hash-table/hash-table.spec.ts
@@ -6,28 +6,76 @@ describe('Dynamic array implementation with doubly linked list', () => {
     expect(() => new HashTable(2.3)).toThrowError('Invalid hash array size value provided');
   });
 
-  it('Adding values into hash table by keys', () => {
-    const hash = new HashTable<any>(10);
+  it('Adding values into a hash table by keys', () => {
+    const hash = new HashTable<string | number | string[]>(10);
 
-    hash.set('foo', 42);
-    hash.set(123, true);
-    hash.set(false, [1, 2, 3]);
-    expect(hash.get('foo')).toBe(42);
-    expect(hash.get(123)).toBe(true);
-    expect(hash.get(false)).toEqual([1, 2, 3]);
+    hash.set('name', 'John');
+    hash.set('age', 22);
+    hash.set('skills', ['html', 'css']);
+    expect(hash.get('name')).toBe('John');
+    expect(hash.get('age')).toBe(22);
+    expect(hash.get('skills')).toEqual(['html', 'css']);
   });
 
-  it('Replacing values in hash table', () => {
-    const hash = new HashTable<any>(10);
+  it('Replacing values in a hash table', () => {
+    const hash = new HashTable<string | number | string[]>(10);
 
-    hash.set('hello', 'world');
-    hash.set(42, true);
-    expect(hash.get('hello')).toBe('world');
-    expect(hash.get(42)).toBe(true);
+    hash.set('name', 'John');
+    hash.set('age', 22);
 
-    hash.set('hello', 'username');
-    hash.set(42, false);
-    expect(hash.get('hello')).toBe('username');
-    expect(hash.get(42)).toBe(false);
+    hash.set('name', 'Paul');
+    hash.set('age', 31);
+    expect(hash.get('name')).toBe('Paul');
+    expect(hash.get('age')).toBe(31);
+  });
+
+  it('Removing values from a hash table', () => {
+    const hash = new HashTable<string | number | string[]>(10);
+
+    hash.set('name', 'John');
+    hash.set('age', 22);
+    expect(hash.remove('name')).toBe(true);
+    expect(hash.remove('age')).toBe(true);
+    expect(hash.get('name')).toBeUndefined();
+    expect(hash.get('age')).toBeUndefined();
+  });
+
+  it('Getting a hash table entries list', () => {
+    const hash = new HashTable<string | number | string[]>(10);
+
+    hash.set('name', 'John');
+    hash.set('age', 22);
+    hash.set('skills', ['html', 'css']);
+
+    const entries = [...hash.entries()];
+    expect(entries).toContainEqual(['name', 'John']);
+    expect(entries).toContainEqual(['age', 22]);
+    expect(entries).toContainEqual(['skills', ['html', 'css']]);
+  });
+
+  it('Getting a hash table keys list', () => {
+    const hash = new HashTable<string | number | string[]>(10);
+
+    hash.set('name', 'John');
+    hash.set('age', 22);
+    hash.set('skills', ['html', 'css']);
+
+    const keys = [...hash.keys()];
+    expect(keys).toContain('name');
+    expect(keys).toContain('age');
+    expect(keys).toContain('skills');
+  });
+
+  it('Getting a hash table values list', () => {
+    const hash = new HashTable<string | number | string[]>(10);
+
+    hash.set('name', 'John');
+    hash.set('age', 22);
+    hash.set('skills', ['html', 'css']);
+
+    const values = [...hash.values()];
+    expect(values).toContain('John');
+    expect(values).toContain(22);
+    expect(values).toContainEqual(['html', 'css']);
   });
 });
--- src/homework-02/hash-table/hash-table.ts
+++ src/homework-02/hash-table/hash-table.ts
@@ -2,9 +2,7 @@ import { DoublyLinkedList } from '../../homework-01';
 import type { HashTable } from './hash-table.interface';
 
 export default class HashTableImpl<T> implements HashTable<T> {
-  #hashArray: DoublyLinkedList<[string, T]>[];
-
-  #hashArraySize: number;
+  #hashArray: DoublyLinkedList<[string, T]>[] = [];
 
   #keysCount: number = 0;
 
@@ -14,21 +12,27 @@ export default class HashTableImpl<T> implements HashTable<T> {
 
   #hashMapCoeff = 37;
 
+  #hashArraySize: number;
+
   constructor(size: number = 10) {
     if (size <= 0 || !Number.isInteger(size)) {
       throw new Error('Invalid hash array size value provided');
     }
 
     this.#hashArraySize = size;
-    this.#hashArray = Array<DoublyLinkedList<[string, T]>>(size);
+    this.#prepareHashArray();
+  }
+
+  get #fillRate() {
+    return this.#keysCount / this.#hashArraySize;
+  }
+
+  #prepareHashArray() {
+    this.#hashArray = Array<DoublyLinkedList<[string, T]>>(this.#hashArraySize);
 
     for (let index = 0; index < this.#hashArray.length; index += 1) {
       this.#hashArray[index] = new DoublyLinkedList<[string, T]>();
     }
-  }
-
-  get fillRate() {
-    return this.#keysCount / this.#hashArraySize;
   }
 
   #hashKeyToIndex(key: string) {
@@ -43,28 +47,63 @@ export default class HashTableImpl<T> implements HashTable<T> {
     return keyToNumberMap;
   }
 
-  set(key: any, value: T) {
+  #checkoutTableRehashing() {
+    const prevHashArray = this.#hashArray;
+    this.#hashArraySize *= this.#hashArrayExtendCoeff;
+    this.#keysCount = 0;
+
+    this.#prepareHashArray();
+
+    for (const list of prevHashArray) {
+      for (const [key, value] of list) {
+        this.set(key, value);
+      }
+    }
+  }
+
+  #getHashArrayList(key: any): [DoublyLinkedList<[string, T]>, string] {
     const stringifiedKey = String(key);
     const index = this.#hashKeyToIndex(stringifiedKey);
     const hashArrayList = this.#hashArray[index];
+
+    return [hashArrayList, stringifiedKey];
+  }
+
+  set(key: any, value: T) {
+    if (this.#fillRate > this.#maxFillRate) {
+      this.#checkoutTableRehashing();
+    }
+
+    const [hashArrayList, stringifiedKey] = this.#getHashArrayList(key);
     const entry = hashArrayList.find(([entryKey]) => entryKey === stringifiedKey);
 
     if (entry) {
       hashArrayList.replace(([entryKey]) => entryKey === stringifiedKey, [stringifiedKey, value]);
     } else {
       hashArrayList.push([stringifiedKey, value]);
+      this.#keysCount += 1;
     }
 
     return this;
   }
 
   get(key: any) {
-    const stringifiedKey = String(key);
-    const index = this.#hashKeyToIndex(stringifiedKey);
-    const hashArrayList = this.#hashArray[index];
+    const [hashArrayList, stringifiedKey] = this.#getHashArrayList(key);
     const entry = hashArrayList.find(([entryKey]) => entryKey === stringifiedKey);
 
     return entry?.value[1];
+  }
+
+  remove(key: any) {
+    const [hashArrayList, stringifiedKey] = this.#getHashArrayList(key);
+    const removedEntry = hashArrayList.remove(([entryKey]) => entryKey === stringifiedKey);
+
+    if (removedEntry) {
+      this.#keysCount -= 1;
+      return true;
+    }
+
+    return false;
   }
 
   *entries() {
