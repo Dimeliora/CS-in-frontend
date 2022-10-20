8f17d4d7c6d4a125032c9d071004a896e9d382a4
Дмитрий Лебедко <Lebedko.D.Y@sber-tech.com>
2022-10-21 00:25:57 +0300

    Added regexp exercises 1-4, unit tests & readme

---
+++ src/homework-04/regexp-exercises/regexp-exercises.ts
@@ -0,0 +1,9 @@
+export const isAlphanumericAndDollar = (string: string): boolean => /^[\w$]+$/g.test(string);
+
+export const splitBy = (string: string): string[] => string.split(/[.,;]| +/g);
+
+export const format = <T extends {}>(template: string, data: T): string =>
+  template.replace(/\$\{(\w+)}/g, (_, group: string) => data[group]);
+
+export const cleanupRepeatingGroups = (string: string): string =>
+  string.replace(/(\w{1,3}?)\1+/g, (_, group: string) => group);
