86d1b802f3452943b673859616cb67d5a595cce6
Дмитрий Лебедко <Lebedko.D.Y@sber-tech.com>
2022-10-21 01:05:22 +0300

    Simplfied replacer in cleanupRepeatingGroups

--- src/homework-04/regexp-exercises/regexp-exercises.ts
+++ src/homework-04/regexp-exercises/regexp-exercises.ts
@@ -5,5 +5,4 @@ export const splitBy = (string: string): string[] => string.split(/[.,;]| +/g);
 export const format = <T extends {}>(template: string, data: T): string =>
   template.replace(/\$\{(\w+)}/g, (_, group: string) => data[group]);
 
-export const cleanupRepeatingGroups = (string: string): string =>
-  string.replace(/(\w{1,3}?)\1+/g, (_, group: string) => group);
+export const cleanupRepeatingGroups = (string: string): string => string.replace(/(\w{1,3}?)\1+/g, '$1');
