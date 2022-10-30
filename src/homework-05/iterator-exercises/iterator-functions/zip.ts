44e8ae5cf78eb01707d02f0e212fd3d2379ecbea
Dimeliora <dimeliora710@gmail.com>
2022-10-30 13:39:45 +0300

    Updated zip behaviour & test

--- src/homework-05/iterator-exercises/iterator-functions/zip.ts
+++ src/homework-05/iterator-exercises/iterator-functions/zip.ts
@@ -1,33 +1,19 @@
 import type { ExtractIterablesType } from '../iterator.types';
 
-export default function zip<T extends Iterable<unknown>[]>(
-  ...iterables: T
-): IterableIterator<ExtractIterablesType<T>[]> {
-  const iterators: Iterator<unknown>[] = [];
+export default function zip<T extends Iterable<any>[]>(...iterables: T): IterableIterator<ExtractIterablesType<T>[]> {
+  const iterators = iterables.map((iter) => iter[Symbol.iterator]());
 
   return {
     next(): IteratorResult<ExtractIterablesType<T>[]> {
-      if (iterables.length === 0) {
-        return { done: true, value: undefined };
-      }
-
-      if (iterators.length === 0) {
-        for (const iterableItem of iterables) {
-          iterators.push(iterableItem[Symbol.iterator]());
-        }
-      }
-
-      let isFinished = true;
       const iteratorsValue: ExtractIterablesType<T>[] = [];
       for (const iteratorItem of iterators) {
         const { done, value } = iteratorItem.next();
+        if (done) return { done: true, value: undefined };
 
-        const typedValue = <ExtractIterablesType<T>>value;
-        if (!done) isFinished = false;
-        iteratorsValue.push(typedValue);
+        iteratorsValue.push(value);
       }
 
-      return isFinished ? { done: true, value: undefined } : { done: false, value: iteratorsValue };
+      return iteratorsValue.length > 0 ? { done: false, value: iteratorsValue } : { done: true, value: undefined };
     },
     [Symbol.iterator](): IterableIterator<ExtractIterablesType<T>[]> {
       return this;
